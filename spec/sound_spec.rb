351a52c80ea657aca7cb6ad33e275ab4bc0bf6a3
Aleksandra <ajmpawlik@gmail.com>
2019-07-31 18:47:18 +0100

    refactor: test for change_frequency method refactored

--- spec/sound_spec.rb
+++ spec/sound_spec.rb
@@ -2,6 +2,6 @@ require 'sound'
 
 describe '#change_frequency' do
   it 'changes the frequencies of the wave' do
-    example(change_frequency([1])).to eq (1)
+    example(change_frequency(1)).to eq (1)
   end
 end
